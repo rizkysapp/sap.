242a2e28c3c752d6aee7bd3d6e5969cdf9239c4f
rizkysap <100526009+Rizsap@users.noreply.github.com>
2023-10-31 16:29:00 +0700

    commit selectMenu 2

--- src/components/selects/SelectMenu.tsx
+++ src/components/selects/SelectMenu.tsx
@@ -1,25 +1,60 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import * as Select from "@radix-ui/react-select";
 import { SelTrigger, Dropdown, Wrapper, Viewport, Item } from "@/app/styles/select";
 import Chevron from "@/app/styles/chevron";
 import { IconType } from "react-icons/lib";
 
-export type item = {
+export interface item {
   id: String;
   name: String;
   icon: IconType;
-};
+}
 
-type SelectProps = {
-  selected: item;
-  setSelected: React.Dispatch<React.SetStateAction<item>>;
+interface Props {
   items: item[];
-};
+  value: String;
+  onChange(value: string): void;
+  name: String;
+  defaultValue: String;
+}
 
 const error = null;
 
-const SelectMenu: React.FC<SelectProps> = ({ items, setSelected, selected }) => {
-  const [toggled, setToggled] = useState("closed");
+const SelectMenu: React.FC<Props> = ({ items, value, onChange, name, defaultValue }) => {
+  const [currentItem, setCurrentItem] = useState<item | undefined>(undefined);
+  const [availableList, setAvailableList] = useState<item[]>([]);
+  const [opened, onOpenChange] = useState<boolean>(false);
+
+  const onValueChange = (value: string) => {
+    if (items) {
+      const matchingValue = items.find((element) => element.id == value);
+      if (matchingValue) {
+        setCurrentItem({ ...matchingValue });
+        setAvailableList(items.filter((i) => i.id != matchingValue.id));
+        onChange && onChange(value);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (defaultValue) {
+      console.log(items, "selectChoices");
+      const newValue = items.find((i) => i.id === defaultValue);
+      console.log(defaultValue, "defaultValue");
+      if (!newValue) {
+        console.error("can not find new value");
+      }
+      setCurrentItem(newValue);
+      console.log(newValue, "new value");
+
+      setAvailableList(items.filter((i) => i.id != defaultValue));
+    }
+  }, [defaultValue, items]);
+
+  useEffect(() => {
+    setAvailableList(items);
+    setCurrentItem(undefined);
+  }, [items]);
 
   return (
     <>
@@ -28,11 +63,13 @@ const SelectMenu: React.FC<SelectProps> = ({ items, setSelected, selected }) => 
           <Select.Root
             dir="ltr"
             // open={true}
-            defaultValue={`${selected.name}`}
-            onOpenChange={(e) => setToggled(e == true ? "open" : "closed")}
-            onValueChange={(e) => (e != selected.id ? setSelected : selected)}
+            defaultValue={`${defaultValue}`}
+            onOpenChange={onOpenChange}
+            onValueChange={onValueChange}
+            name={`${name}`}
+            value={`${value}`}
           >
-            <Select.Trigger asChild data-state={setSelected}>
+            <Select.Trigger asChild data-state={onOpenChange}>
               <SelTrigger error={!!error}>
                 <span>
                   <Select.Value />
@@ -45,7 +82,7 @@ const SelectMenu: React.FC<SelectProps> = ({ items, setSelected, selected }) => 
             <Select.Content asChild>
               <Dropdown>
                 <Viewport>
-                  {items.map((item, i) => {
+                  {availableList.map((item, i) => {
                     return (
                       <Item key={i} value={`${item.name}`}>
                         <Select.ItemText> {item.name} </Select.ItemText>
